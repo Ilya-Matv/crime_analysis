7ffee22bbc8ee0d5675f7b60e7ecb8c025e03720
Ilya <67767346+Ilya-Matv@users.noreply.github.com>
2024-09-11 05:32:16 -0600

    Update Coordinate_statistics.sql

--- Coordinate_statistics.sql
+++ Coordinate_statistics.sql
@@ -4,33 +4,37 @@ WITH crime_data AS (
     longitude,
     block,
     primary_type,
+    location_description,
     year
   FROM 
     `bigquery-public-data.chicago_crime.crime`
   WHERE 
-    year >=2023  -- Filter for the past year
+    year >= 2023
     AND latitude IS NOT NULL
     AND longitude IS NOT NULL
 ),
 filtered_coordinates AS (
   SELECT 
-    latitude,
-    longitude,
-    block,
+    location_description,
     primary_type,
     COUNT(*) AS crime_count
   FROM (
     SELECT 
       *,
-      ST_DISTANCE(ST_GEOGPOINT(longitude, latitude), ST_GEOGPOINT(-87.6278, 41.8835)) AS distance
+      ST_DISTANCE(ST_GEOGPOINT(longitude, latitude), ST_GEOGPOINT(-87.627876698, 41.883500187)) AS distance
     FROM 
       crime_data
   )
   WHERE distance <= 1000 -- 1 km radius in meters
   GROUP BY 
-    latitude, longitude, block, primary_type
+    location_description, primary_type
   ORDER BY 
     crime_count DESC
 )
-SELECT * 
-FROM filtered_coordinates;
+SELECT 
+  location_description, 
+  primary_type, 
+  crime_count
+FROM 
+  filtered_coordinates
+LIMIT 10;
